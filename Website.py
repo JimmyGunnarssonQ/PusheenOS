1dd985f81550d31779212a95b670c41837259849
Lorago <v.a@hotmail.se>
2021-10-16 19:12:55 +0200

    Update Website.py

--- Website.py
+++ Website.py
@@ -1,3 +1,5 @@
 import webbrowser
 while True:
+    print("PUSHEEN is a trademark of Pusheen Corp.")
+    print("Make sure to donate to the Abhijeet Pusheen problem foundation, to make PhD level math problems freely available for preschoolers")
     webbrowser.open("https://www.pusheen.com")
